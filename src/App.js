9fc1b53971aa9d0154436b8a4af36babd500b264
Danyel TZ <slbdanyeltz@gmail.com>
2021-04-09 18:38:39 -0500

    inicio calculadora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import { Container, Grid, TextField, Button } from '@material-ui/core';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <code>Daniel Ricaurte</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default class App extends React.Component {
+
+   constructor(props) {
+    super(props);
+    this.state = {
+      campo:'',
+      operacion:'',
+      resultado:''
+    }
+  }
+
+  clear() {
+    this.setState()
+  } 
+}
+
+
+
+
+
   );
 }
 
